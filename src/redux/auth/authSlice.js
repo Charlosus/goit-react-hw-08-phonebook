f5a17f74ba6ff358d1629ee892355696250eb1a8
Charlosus <145377783+Charlosus@users.noreply.github.com>
2025-06-20 16:22:50 +0200

    Update authSlice.js

---
+++ src/redux/auth/authSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+import {register, logIn, logOut, refreshUser} from './authOperations';
+
+const iniitialState = {
+    user: {name: null, email: null},
+    token: null,
+    isLoggedIn: false,
+    isRefreshing: false,
+};
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    extraReducers: builder => {
+        builder 
+        .addCase(register.fulfilled, (state, action) =>)
+    }
+})
