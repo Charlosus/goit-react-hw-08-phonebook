7084dd20ef41b97a530af4a6f5efb07741b48de5
Charlosus <145377783+Charlosus@users.noreply.github.com>
2025-06-20 17:16:09 +0200

    0.32v

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -1,35 +1,38 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-axios.defaults.baseURL = 'https://connections-api.goit.global/'
+axios.defaults.baseURL = 'https://connections-api.goit.global/';
 
-const setAuthHeader = (token) => {
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-}
+const setAuthHeader = token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
 
 const clearAuthHeader = () => {
-    axios.defaults.headers.common.Authorization ='';
-}
+  axios.defaults.headers.common.Authorization = '';
+};
 
 export const register = createAsyncThunk(
-    'auth/register', 
-    async(credentials, thunkAPI) => {
-        try {
-            const response = await axios.post('/users/signup', credentials);
-            setAuthHeader(response.data.token);
-            return response.data;
-        } catch (err) {
-            return thunkAPI.rejectWithValue(err.message);
-        } 
+  'auth/register',
+  async (credentials, thunkAPI) => {
+    try {
+      const response = await axios.post('/users/signup', credentials);
+      setAuthHeader(response.data.token);
+      return response.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.message);
     }
-)
+  }
+);
 
 export const logIn = createAsyncThunk(
-    "auth/logIn",
-    async (credentials, thunkAPI) => {
-        try {
-            const response = await axios.post('/users/login', credentials);
-            setAuthHeader(response.data.token);
-        }
+  'auth/logIn',
+  async (credentials, thunkAPI) => {
+    try {
+      const response = await axios.post('/users/login', credentials);
+      setAuthHeader(response.data.token);
+      return response.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.message);
     }
-)
+  }
+);
