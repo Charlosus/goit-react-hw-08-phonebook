2221a4df00079903a7dda93ea5ec0484a3b8b5f3
Charlosus <145377783+Charlosus@users.noreply.github.com>
2025-06-20 17:04:53 +0200

    Update RegisterForm.jsx

--- src/pages/Register/RegisterForm.jsx
+++ src/pages/Register/RegisterForm.jsx
@@ -1,2 +1,36 @@
-import { useDispatch} from "react-redux";
-import register
+import { useDispatch } from 'react-redux';
+import { register } from '../../redux/auth/authOperations';
+
+export default function RegisterForm() {
+  const dispatch = useDispatch();
+  const handleSubmit = e => {
+    e.preventDeault();
+    const form = e.target;
+    const { name, email, password } = form.elements;
+    dispatch(
+      register({
+        name: name.value,
+        email: email.value,
+        password: password.value,
+      })
+    );
+    form.reset();
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name
+        <input type="text" name="name" />
+      </label>
+      <label>
+        Email
+        <input type="email" name="email" />
+      </label>
+      <label>
+        Password
+        <input type="password" name="password" />
+      </label>
+      <button typ="submit">Register</button>
+    </form>
+  );
+}
